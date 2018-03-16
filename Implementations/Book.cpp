9b6146d146f06232822a1a4817ce8a5cb311bd8f
clarson1229 <27305718+clarson1229@users.noreply.github.com>
2018-03-16 18:57:29 -0500

    Defined the Book class.

--- Implementations/Book.cpp
+++ Implementations/Book.cpp
@@ -1,10 +1,29 @@
 #include "../Headers/Book.h"
 
-Book::Book(std::string name) : _name(name)
-{}
-
-
-int main()
+Book::Book(std::string name)
 {
+    _name = name;
+    _isArchived = false;
+    _circulationStartDate = Date();
+    _circulationEndDate = Date();
+    _empQueue = PriorityQueue();
+
+}
+
+void Book::pushEmployee(Employee e) {
+    _empQueue.push(e);
+
+}
+
+void Book::popEmployee() {
+    _empQueue.pop();
+
+}
+Employee Book::frontEmployee() {
+    return _empQueue.front();
+
+}
+bool Book::operator==(const Book &rhs) const {
+    return (_name == rhs._name);
 
 }
